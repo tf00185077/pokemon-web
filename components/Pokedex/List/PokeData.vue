429a217e48b5b870ddd770b0ab9952f2d099907f
unknown <tf00185077@gmail.com>
2023-09-21 16:13:49 +0800

    deck select effect

--- components/Pokedex/List/PokeData.vue
+++ components/Pokedex/List/PokeData.vue
@@ -9,26 +9,7 @@ const pokeName = dataDetail.name
 const pokeTypes = dataDetail.types
 const pokeTypeArray = pokeTypes.split('\n').filter(item=>item.trim() !== '') //將複數屬性整理成陣列
 // console.log(pokeTypeArray)
-const chineseToEnglish = {
-    "一般":"normal",
-    "火":"fire",
-    "水":"water",
-    "電":"electric",
-    "草":"grass",
-    "冰":"ice",
-    "格鬥":"fighting",
-    "毒":"poison",
-    "地面":"ground",
-    "飛行":"flying",
-    "超能力":"psychic",
-    "蟲":"bug",
-    "岩石":"rock",
-    "幽靈":"ghost",
-    "龍":"dragon",
-    "惡":"dark",
-    "鋼":"steel",
-    "妖精":"fairy"
-}
+const chineseToEnglish = useTypeToEnglish()
 </script>
 <template>
         <div class="  data-card-layout">
---
+++ composables/useTypeToEnglish.js
@@ -0,0 +1,22 @@
+export const useTypeToEnglish =() => {
+   return (
+    {"一般":"normal",
+    "火":"fire",
+    "水":"water",
+    "電":"electric",
+    "草":"grass",
+    "冰":"ice",
+    "格鬥":"fighting",
+    "毒":"poison",
+    "地面":"ground",
+    "飛行":"flying",
+    "超能力":"psychic",
+    "蟲":"bug",
+    "岩石":"rock",
+    "幽靈":"ghost",
+    "龍":"dragon",
+    "惡":"dark",
+    "鋼":"steel",
+    "妖精":"fairy"}
+    )
+}
