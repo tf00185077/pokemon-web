d43d179ddab09e866f4a827bd583fbc201d9ab4a
unknown <tf00185077@gmail.com>
2023-09-23 23:04:35 +0800

    small fix about header

--- app.vue
+++ app.vue
@@ -1,17 +1,22 @@
 <script setup>
   useHead({
     bodyAttrs:{
-      style:"margin:0;padding:0;position:relative;background:#091521"
+      style:"margin:0;padding:0;position:relative;background:#091521;margin-top:70px;"
     }
   })
 </script>
 <template>
   <div class="layout">
-    <!-- <Header class="header"></Header> -->
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
+    <Header class="header"></Header>
+      <NuxtPage/>
+    <Footer></Footer>
   </div>
 </template>
 <style scoped>
+.header{
+  position:fixed;
+  width:100%;
+  z-index:1000;
+  top:0;
+}
 </style>
